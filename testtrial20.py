c99d75a358affb8f37633e8946d82cd2ae17cfca
Michael Mutinda <mutindamike@gmail.com>
2019-10-24 16:16:00 +0300

    tested my function and it worked

--- testtrial20.py
+++ testtrial20.py
@@ -1,17 +1,17 @@
 import unittest
-import trial20
+import trial20, mikeprime
 
 
 class TestCases(unittest.TestCase):
 
     def testPrimeNumbers(self):
-        self.assertTrue(trial20.isPrime(2))
-        self.assertTrue(trial20.isPrime(3))
-        self.assertTrue(trial20.isPrime(5))
-        self.assertTrue(trial20.isPrime(7))
-        self.assertTrue(trial20.isPrime(11))
-        self.assertTrue(trial20.isPrime(19))
-        self.assertTrue(trial20.isPrime(29))
+        self.assertTrue(mikeprime.isPrime(2))
+        self.assertTrue(mikeprime.isPrime(3))
+        self.assertTrue(mikeprime.isPrime(5))
+        self.assertTrue(mikeprime.isPrime(7))
+        self.assertTrue(mikeprime.isPrime(11))
+        self.assertTrue(mikeprime.isPrime(19))
+        self.assertTrue(mikeprime.isPrime(29))
 
 
 if __name__ == '__main__':
